541d2cc6c63da94ebc3747f2bfeb7dea020325a1
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-29 20:25:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main = document.querySelector('main#main')
+main.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.id= 'victory'
+
+newHeader.innerHTML = "Cedric is the champion"
