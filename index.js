28ca4253b9da79b5a7028dc4ffeac4956fca5585
danielkwon89 <danielkwon89@gmail.com>
2020-11-07 21:57:58 -0500

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,5 @@
 document.getElementById('main').remove();
 
 let newHeader = document.createElement('h1');
-newHeader.id = "victory";
-newHeader.innerText = "Daniel is the champion";
-document.body.appendChild(newHeader);
-newHeader = document.getElementById('victory');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Daniel is the champion';
--- test/indexTest.js
+++ test/indexTest.js
@@ -12,9 +12,9 @@ describe("index.html", () => {
       expect(newHeader.id, "Make sure you create an <h1> with id 'victory'").eql('victory')
     });
 
-    // it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
-    //   expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
-    // });
+    it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
+      expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+    });
 
   });
 })
