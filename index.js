73151ec58eaaf2a0caf2d50fd44bf80f385fa75b
danielkwon89 <danielkwon89@gmail.com>
2020-11-07 21:48:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerText = "Daniel is the champion";
+document.body.appendChild(newHeader);
+newHeader = document.getElementById('victory');
