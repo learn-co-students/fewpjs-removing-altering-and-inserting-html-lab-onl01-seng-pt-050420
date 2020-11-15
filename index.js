01b4904726ea51a25227536ab197c5f35534a4a5
dangwin <danghwin@gmail.com>
2020-11-15 10:11:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+var el = document.getElementById('main');
+el.remove(); 
+
+let newHeader = document.createElement('h1');
+let attr = document.createAttribute("id");
+attr.value = "victory";
+newHeader.setAttributeNode(attr);
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "DANG is the champion";
+
