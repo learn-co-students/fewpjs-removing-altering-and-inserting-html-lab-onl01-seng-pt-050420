c06664ad3e8c89ea76bd438486b4cc11f30e0c7c
Awilliams4321 <awilliams1234@live.com>
2020-11-15 11:20:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementById('main')
+
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Amber is the champion"
+
+const body = document.getElementsByTagName('body')[0]
+body.appendChild(newHeader)
+
+
